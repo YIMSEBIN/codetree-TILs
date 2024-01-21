ebd92746735e161147cfda49b38840f416d35692
YIMSEBIN <82010185+YIMSEBIN@users.noreply.github.com>
2024-01-21 21:40:40 +0900

    Title: 그 계절, 그 날, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240121/그 계절, 그 날/that-season-that-day.py
+++ 240121/그 계절, 그 날/that-season-that-day.py
@@ -1,15 +1,61 @@
 def func(y, m, d) :
-    if m == 2 and d == 29 :
-        if (y%100 == 0 and y%400 != 0) or (y%4 != 0) :
+    if m == 1 :
+        if d > 31 :
             return -1
-    if 3<= m <= 5 :
-        return "Spring"
-    elif 6<= m <= 8 :
-        return "Summer"
-    elif 9<= m <= 11 :
-        return "Fall"
-    else :
-        return "Winter"
+        else :
+            return "Spring"
+    elif m == 2 :
+        if d == 29 and ((y%100 == 0 and y%400 != 0) or (y%4 != 0)) :
+            return -1
+        if d > 29 :
+            return -1
+        else :
+            return "Spring"
+    elif m == 3 :
+        if d > 31 :
+            return -1
+        else :
+            return "Spring"
+    elif m == 4 :
+        if d > 30 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 5 :
+        if d > 31 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 6 :
+        if d > 30 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 7 and m == 8 :
+        if d > 31 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 9 :
+        if d > 30 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 10 :
+        if d > 31 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 11 :
+        if d > 30 :
+            return -1
+        else :
+            return "Summer"
+    elif m == 12 :
+        if d > 31 :
+            return -1
+        else :
+            return "Summer"
 
 
 y, m, d = map(int, input().split())
