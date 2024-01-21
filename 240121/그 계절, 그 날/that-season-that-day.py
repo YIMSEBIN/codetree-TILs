b989802478552f5db3edc4b82d2775543462cecd
YIMSEBIN <82010185+YIMSEBIN@users.noreply.github.com>
2024-01-21 21:36:23 +0900

    Title: 그 계절, 그 날, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240121/그 계절, 그 날/that-season-that-day.py
+++ 240121/그 계절, 그 날/that-season-that-day.py
@@ -1,12 +1,6 @@
 def func(y, m, d) :
     if m == 2 and d == 29 :
-        print("here")
-        print(y % 400)
-        print(y % 4)
-        print(y % 100)
-        if y % 400 == 0 or (y % 4 == 0 and y % 100 != 0) :
-            continue
-        else :
+        if (y%100 == 0 and y%400 != 0) or (y%4 != 0) :
             return -1
     if 3<= m <= 5 :
         return "Spring"
@@ -15,7 +9,7 @@ def func(y, m, d) :
     elif 9<= m <= 11 :
         return "Fall"
     else :
-        return "winter"
+        return "Winter"
 
 
 y, m, d = map(int, input().split())
