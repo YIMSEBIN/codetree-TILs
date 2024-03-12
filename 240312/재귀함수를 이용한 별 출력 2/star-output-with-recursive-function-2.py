6a53dca5e386539e229482b0cb1fd66f2dd06a1d
YIMSEBIN <82010185+YIMSEBIN@users.noreply.github.com>
2024-03-12 17:49:25 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 198ms, Memory: 26MB, Status: Passed - CodeTree

--- 240312/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 240312/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -1,14 +1,22 @@
-def foo(a, b, c) :
-    if b == 2*c :
-        return
-    print(a*"* ")
-    b = b + 1
-    if b < c :
-        foo(a-1, b, c)
-    elif b == c :
-        foo(a, b, c)
-    else :
-        foo(a+1, b, c)
-
+# 변수 선언 및 입력:
 n = int(input())
-foo(n, 0, n)
+
+
+def print_star(n):   # 1부터 2n번째 줄까지 주어진 모양대로 별을 출력하는 함수
+    if n == 0:       # n이 0이라면, 더 이상 진행하지 않고
+        return       # 퇴각합니다.
+    
+    # 외각 별을 출력합니다.
+    for _ in range(n):
+        print("*", end=" ")
+    print()
+
+    print_star(n - 1) # 가운데 별을 출력하는 함수
+
+    # 외각 별을 출력합니다.
+    for _ in range(n):
+        print("*", end=" ")
+    print()
+
+   
+print_star(n)
