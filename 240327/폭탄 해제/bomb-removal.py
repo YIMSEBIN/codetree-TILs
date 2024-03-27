b7657349f1eb8cffdc87ddb8644facd1d3c11004
YIMSEBIN <82010185+YIMSEBIN@users.noreply.github.com>
2024-03-27 17:32:13 +0900

    Title: 폭탄 해제, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/폭탄 해제/bomb-removal.py
@@ -0,0 +1,11 @@
+class Bomb :
+    def __init__(self, code, color, second) :
+        self.code = code
+        self.color = color
+        self.second = second
+
+code, color, second = map(str, input().split())
+bomb = Bomb(code, color, int(second))
+print("code : %s\n",code)
+print("color : %s\n",color)
+print("second : %d\n",second)
