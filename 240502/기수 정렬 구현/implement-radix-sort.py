f8f34155a46e6dd86b5b6a403611f72754cce15d
YIMSEBIN <82010185+YIMSEBIN@users.noreply.github.com>
2024-05-02 20:56:19 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240502/기수 정렬 구현/implement-radix-sort.py
+++ 240502/기수 정렬 구현/implement-radix-sort.py
@@ -6,9 +6,12 @@ for i in nums :
         pos = len(i)
 
 for i in range(pos) :
+    for j in range(n) :
+        if len(nums[j]) < i+1 :
+            nums[j] = 0 + nums[j]
     arr_new = [[] for _ in range(10)]
     for j in range(n) :
-        arr_new[int(nums[j][pos-1])] = nums[j]
+        arr_new[int(nums[j][i])] = nums[j]
     store_arr = []
     for j in range(10) :
         store_arr += arr_new[j]
