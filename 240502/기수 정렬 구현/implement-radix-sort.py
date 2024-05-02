97befda3e185dcc0bbeeb828df05431cb44cf5f1
YIMSEBIN <82010185+YIMSEBIN@users.noreply.github.com>
2024-05-02 21:25:05 +0900

    Title: 기수 정렬 구현, Time: 332ms, Memory: 41MB, Status: Passed - CodeTree

--- 240502/기수 정렬 구현/implement-radix-sort.py
+++ 240502/기수 정렬 구현/implement-radix-sort.py
@@ -4,17 +4,21 @@ pos = 0
 for i in nums :
     if pos < len(i) :
         pos = len(i)
+for i in range(n) :
+    while len(nums[i]) < pos :
+        nums[i] = '0' + nums[i]
 
-for i in range(pos) :
-    for j in range(n) :
-        if len(nums[j]) < i+1 :
-            nums[j] = 0 + nums[j]
+for i in range(pos-1, -1, -1) :    
     arr_new = [[] for _ in range(10)]
     for j in range(n) :
-        arr_new[int(nums[j][i])] = nums[j]
+        arr_new[int(nums[j][i])].append(nums[j])
     store_arr = []
     for j in range(10) :
-        store_arr += arr_new[j]
+        if len(arr_new[j]) != 0 :
+            store_arr += arr_new[j]
     nums = store_arr
 
+for i in range(n) :
+    while nums[i][0] == '0' :
+        nums[i] = nums[i][1:]
 print(*nums)
